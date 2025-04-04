d1a3f8660db40f3d8af55f559e2d7d02e2b63c35
Pedrosmn <128317304+Pedrosmn@users.noreply.github.com>
2025-04-03 21:16:34 -0300

    Update api.ts

--- src/api.ts
+++ src/api.ts
@@ -1,13 +1,15 @@
+const API_URL = import.meta.env.VITE_API_URL; // Para Vite
+// const API_URL = process.env.REACT_APP_API_URL; // Para Create React App
+
 export const fetchAlgorithms = async () => {
-    try {
-      const response = await fetch("http://127.0.0.1:5000/algorithms");
-      if (!response.ok) {
-        throw new Error("Erro ao buscar os algoritmos");
-      }
-      return await response.json();
-    } catch (error) {
-      console.error(error);
-      return [];
+  try {
+    const response = await fetch(`${API_URL}/algorithms`);
+    if (!response.ok) {
+      throw new Error("Erro ao buscar os algoritmos");
     }
-  };
-  
+    return await response.json();
+  } catch (error) {
+    console.error("Erro na requisição:", error);
+    return [];
+  }
+};
